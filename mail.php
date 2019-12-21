0ed1bf1d703ef792e31a444ed4c544da2586c848
danielozac <emanuela_dudau@yahoo.com>
2019-12-21 15:44:16 +0000

    add contact page

--- mail.php
+++ mail.php
@@ -16,23 +16,23 @@ $variable = <<<XYZ
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>Bootstrap 4 Thank You Page Template</title>
-  <script src="https://bootstrapcreative.com/wp-bc/wp-content/themes/wp-bootstrap/codepen/bootstrapcreative.js?v=7"></script><link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'><link rel="stylesheet" href="./style.css">
+  <title>Email sent</title>
+  <script src="https://bootstrapcreative.com/wp-bc/wp-content/themes/wp-bootstrap/codepen/bootstrapcreative.js?v=7"></script>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+  <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
-<!-- partial:index.partial.html -->
 <div class="jumbotron text-center">
   <h1 class="display-3">Thank You!</h1>
-  <p class="lead"><strong>I will</strong> be contacting you soon.</p>
+  <p class="lead">I will be contacting you soon.</p>
   <hr>
   <p class="lead">
-    <a class="btn btn-primary btn-sm" href="index.html" role="button">Continue to homepage</a>
+    <a class="btn btn-primary btn-sm" href="http://emanueladudau.com/" role="button">Continue to homepage</a>
   </p>
 </div>
-<!-- partial -->
-  <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'></script>
-<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js'></script>
+
+
 </body>
 </html>
 XYZ;
